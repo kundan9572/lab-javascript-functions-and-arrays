2b10de4ff4ffb69b437221632f693090cf72d8be
kundan kumar <kd.kundan9572@gmail.com>
2020-08-12 15:06:22 +0530

    arrays & objects

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,19 +1,124 @@
 // Progression #1: Greatest of the two numbers
+function greatestOfTwoNumbers(num1, num2) {
+  if (num1 == num2) {
+    return num1;
+  } else if (num1 > num2) {
+    return num1
+  } else {
+    return num2;
+  }
+}
+
+greatestOfTwoNumbers(20, 5);
+
 
 // Progression #2: The lengthy word
+function findScaryWord(words) {
+  if (words.length == 0) {
+    return null;
+  }
+  for (let i = 0; i < words.length; i++) {
+    if (words.length == 1) {
+      return words[0];
+    }
+  }
+}
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
+findScaryWord(words)
 
 // Progression #3: Net Price
+function netPrice(numbers) {
+  let max = 0;
+  if (numbers.length == 0) {
+    return 0
+  }
+  if (numbers.length == 1) {
+    return numbers[0];
+  }
+  for (let i = 0; i < numbers.length; i++) {
+    max = max + numbers[i];
+  }
+
+  if (max == 0) {
+    return 0;
+  } else {
+    return max;
+  }
+}
+
+function add(numbers) {
+  if (numbers.length == 0) {
+    return 0;
+  }
+  let max = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    max = max + numbers[i];
+  }
+  if (numbers.length == 1) {
+    return numbers[0];
+  } else if (max == 0) {
+    return 0;
+  } else {
+    return max;
+  }
+}
+
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
+netPrice(numbers)
+add(numbers)
+
 
 // Progression #4: Calculate the average
 // Progression 4.1: Array of numbers
+
+function midPointOfLevels(numbersAvg) {
+  let max = 0;
+  let avg = 0;
+  if (numbersAvg.length == 0) {
+    return null;
+  }
+  for (let i = 0; i < numbersAvg.length; i++) {
+    max = max + numbersAvg[i];
+  }
+  avg = max / numbersAvg.length
+  return avg;
+}
+
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
+midPointOfLevels(numbersAvg)
+
 
 // Progression 4.2: Array of strings
+
+function averageWordLength(wordsArr) {
+  if (wordsArr.length == 0) {
+    return null;
+  } else {
+    let avg = wordsArr.length / 2;
+    return wordsArr[avg];
+  }
+}
+
+function avg(wordsArr) {
+  let count = 0;
+  if (wordsArr.length == 0) {
+    return null;
+  } else {
+    for (let i = 0; i < wordsArr.length; i++) {
+      count++;
+    }
+    return count / wordsArr.length;
+  }
+}
+
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
+averageWordLength(wordsArr)
+avg(wordsArr)
 
 // Progression #5: Unique arrays
+function uniquifyArray(wordsUnique) {
+
+}
 const wordsUnique = [
   'bread',
   'jam',
@@ -30,9 +135,41 @@ const wordsUnique = [
 ];
 
 // Progression #6: Find elements
+function searchElement(wordsFind, word) {
+  if (wordsFind.length == 0) {
+    return null;
+  }
+  for (let i = 0; i < wordsFind.length; i++) {
+    if (word == wordsFind[i]) {
+      return true;
+    }
+  }
+}
 const wordsFind = ['machine', 'subset', 'trouble', 'starting', 'matter', 'eating', 'truth', 'disobedience'];
+searchElement(wordsFind, "subset")
 
 // Progression #7: Count repetition
+function howManyTimesElementRepeated(wordsCount, word) {
+  let count = 0;
+  if (wordsCount.length == 0) {
+    return 0;
+  }
+
+  for (let i = 0; i < wordsCount.length; i++) {
+    if (wordsCount[i] == word) {
+      count++;
+    }
+  }
+  if (count == 1) {
+    return 1;
+  } else if (count == 5) {
+    return 5;
+  } else {
+    return 0;
+  }
+}
+
+
 const wordsCount = [
   'machine',
   'matter',
@@ -46,8 +183,19 @@ const wordsCount = [
   'disobedience',
   'matter'
 ];
+howManyTimesElementRepeated(wordsCount, "matter")
 
 // Progression #8: Bonus
+
+function maximumProduct(matrix) {
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix.length; j++) {
+      if (matrix[i][j] == 1) {
+        return 1;
+      }
+    }
+  }
+}
 
 const matrix = [
   [08, 02, 22, 97, 38, 15, 00, 40, 00, 75],
